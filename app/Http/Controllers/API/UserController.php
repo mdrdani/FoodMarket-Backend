3bd6cc464cc177dc7361e06f34d188ba80c6110b
mdrdani <sudokill3004@gmail.com>
2021-12-23 21:51:38 +0700

    API Register

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Actions\Fortify\PasswordValidationRules;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     //
+
+    use PasswordValidationRules;
+
     public function login(Request $request)
     {
         try{
@@ -48,6 +52,43 @@ class UserController extends Controller
         }catch(Exception $error){
             return ResponseFormatter::error([
                 'message' => 'Something wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        try {
+            //code...
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => $this->passwordRules()
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'address' => $request->address,
+                'houseNumber' => $request->houseNumber,
+                'phoneNumber' => $request->phoneNumber,
+                'city' => $request->city,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+            $tokenResult = $user->createToken('authToken')-> plainTextToken;
+
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ]);
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something Went Wrong',
                 'error' => $error
             ], 'Authentication Failed', 500);
         }

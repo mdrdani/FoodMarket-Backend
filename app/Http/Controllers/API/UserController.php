86fa5c102544e7264432a6ef72bd230a9ae3cc9f
mdrdani <sudokill3004@gmail.com>
2021-12-24 19:44:30 +0700

    API Upload Foto dan Route User API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -11,6 +11,7 @@ use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -105,7 +106,7 @@ class UserController extends Controller
     {
         return ResponseFormatter::success($request->user(), 'Data User Berhasil Di ambil');
     }
-    
+
     public function updateProfile(Request $request)
     {
         $data = $request->all();
@@ -115,5 +116,31 @@ class UserController extends Controller
         $user->update($data);
         
         return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if($validator->fails())
+        {
+            return ResponseFormatter::error([
+                'error' => $validator->errors()
+            ], 'Update photo fails', 401);
+        }
+
+        if($request->file('file'))
+        {
+            $file = $request->file->store('assets/user', 'public');
+
+            // simpan foto ke database (url)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File Successfuly uploaded');
+        }
     }
 }

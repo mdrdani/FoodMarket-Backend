cde3b136675ab34299b9be870188deaa48df5b68
mdrdani <sudokill3004@gmail.com>
2022-03-13 11:13:04 +0700

    Halaman Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +15,15 @@ use App\Http\Controllers\API\MidtransController;
 |
 */
 
-
+// Homepage
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
-Route::get('/debug-sentry', function () {
-    throw new Exception('My first Sentry error!');
+// Dashboard
+Route::prefix('dashboard')->middleware(['auth:sanctum','admin'])->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
 // midtrans related
 Route::get('midtrans/success', [MidtransController::class, 'success']);
